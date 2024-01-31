21138f1b4e5847d5876e4dda66f8a6323cdedb34
moremig29 <miguelangel.moreira@gmail.com>
2024-01-31 11:16:37 -0400

    fin seccion 18

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,5 +1,7 @@
 const { response } = require( 'express' );
 const jwt = require('jsonwebtoken');
+const Usuario = require('../models/usuario.model')
+
 
 const validarJWT = ( req, res = response, next ) => {
 
@@ -32,6 +34,78 @@ const validarJWT = ( req, res = response, next ) => {
 
 }
 
+const validarADMIN_ROLE = async ( req, res = response, next ) => {
+
+  const uid = req.uid;
+
+  try {
+
+    const usuarioDB = await Usuario.findById(uid);
+
+    if ( !usuarioDB ) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'usuario no existe'
+      });
+    }
+
+    if ( usuarioDB.role !== 'ADMIN_ROLE' ) {
+      return res.status(403).json({
+        ok: false,
+        msg: 'usuario no existe'
+      });
+    }
+
+    next();
+    
+  } catch (error) {
+    console.log( error )
+    res.status(500).json({
+      ok: false,
+      msg: 'hable con el administrador'
+    })
+  }
+
+}
+
+const validarADMIN_ROLE_o_MismoUsuario = async ( req, res = response, next ) => {
+
+  const uid = req.uid;
+  const id = req.params.id;
+
+  try {
+
+    const usuarioDB = await Usuario.findById(uid);
+
+    if ( !usuarioDB ) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'usuario no existe'
+      });
+    }
+
+    if ( usuarioDB.role === 'ADMIN_ROLE' || uid === id ) {
+      
+      next();
+      
+    } else {
+      return res.status(403).json({
+        ok: false,
+        msg: 'usuario no existe'
+      });
+    }
+
+    
+  } catch (error) {
+    console.log( error )
+    res.status(500).json({
+      ok: false,
+      msg: 'hable con el administrador'
+    })
+  }
+
+}
+
 module.exports = {
-  validarJWT
+  validarJWT, validarADMIN_ROLE, validarADMIN_ROLE_o_MismoUsuario
 }
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -4,16 +4,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarJWT, validarADMIN_ROLE, validarADMIN_ROLE_o_MismoUsuario } = require('../middlewares/validar-jwt');
 
 const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios.controller');
 
 
 const router = Router();
 
-router.get( '/', validarJWT, getUsuarios );
+router.get( '/', [validarJWT, validarADMIN_ROLE], getUsuarios );
 
 router.post( '/', [
+  validarADMIN_ROLE,
   check('nombre', 'El nombre es obligatorio').not().isEmpty(),
   check('password', 'El password es obligatorio').not().isEmpty(),
   check('email', 'El email es obligatorio').isEmail(),
@@ -22,13 +23,14 @@ router.post( '/', [
 
 router.put( '/:id', [
   validarJWT,
+  validarADMIN_ROLE_o_MismoUsuario,
   check('nombre', 'El nombre es obligatorio').not().isEmpty(),
   check('role', 'El role es obligatorio').not().isEmpty(),
   check('email', 'El email es obligatorio').isEmail(),
   validarCampos
 ], actualizarUsuario );
 
-router.delete( '/:id', validarJWT, borrarUsuario );
+router.delete( '/:id', [validarJWT, validarADMIN_ROLE], borrarUsuario );
 
 
 module.exports = router;

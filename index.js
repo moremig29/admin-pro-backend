9ca5ae9569d6fb0643fa1b456e18354fa1abea5e
moremig29 <miguelangel.moreira@gmail.com>
2026-03-01 14:44:13 -0400

    version lista para produccion

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors')
@@ -27,6 +28,12 @@ app.use( '/api/medicos', require('./routes/medicosRoutes'));
 app.use( '/api/todo', require('./routes/busquedasRoutes'));
 app.use( '/api/upload', require('./routes/uploadsRoutes'));
 app.use( '/api/login', require('./routes/authRoutes'));
+
+// lo ultimo
+
+app.use( '*', ( req, res ) => {
+  res.sendFile( path.resolve( __dirname, 'public/index.html')  );
+})
 
 app.listen( process.env.PORT, () => {
   console.log( 'Servidor en el puerto ' + process.env.PORT );
